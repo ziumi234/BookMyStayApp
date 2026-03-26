df69d2c4eff79b018774efcb4765f162211cb6b4
ziumi234 <uk1486@srmist.edu.in>
2026-03-26 09:42:06 +0530

    Use Case 1: Application Entry & Welcome Message

---
+++ BookMyStayApp/src/BookMyStayApp.java
@@ -0,0 +1,22 @@
+public class BookMyStayApp {
+
+    /**
+     * Application entry point.
+     * This method is the first method executed
+     * when the program is launched by the JVM.
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("Welcome to the Hotel Booking Management System");
+
+        // Simulate system initialization
+        initializeSystem();
+    }
+
+    // Method to initialize system
+    public static void initializeSystem() {
+        System.out.println("System initialized successfully.");
+    }
+}
